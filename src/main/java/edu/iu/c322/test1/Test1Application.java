e7b8fc24c355c534cb4626b74ee00ad1c49bca95
Ethan bridges <eebridge@iu.edu>
2024-03-06 22:12:49 -0500

    test2 backend

--- src/main/java/edu/iu/c322/test1/Test1Application.java
+++ src/main/java/edu/iu/c322/test1/Test1Application.java
@@ -2,7 +2,9 @@ package edu.iu.c322.test1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @SpringBootApplication
 public class Test1Application {
 
